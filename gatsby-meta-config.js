d243e7b6e502574faf755114694cc1934f89e906
Jinho Ahn <jinoan89@gmail.com>
2020-10-16 10:46:21 +0900

    modified gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Jinote`,
+  description: `Blog posted about learn, dev, etc.`,
+  author: `jinoan`,
+  introduction: `Junior AI Engineer`,
+  siteUrl: `https://jinoan.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `jinoan`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    facebook: `h2oinjan`, // Your Facebook account
+    linkedin: `jinho-ahn-998503117`, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'jinoan/blog-comment', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'jinoan',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
